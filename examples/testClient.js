c401a0753f52c3cbc3f08250786a5b8aa47df189
Clint Martin <cmartin0417@gmail.com>
2016-05-27 13:55:24 -0400

    - Code cleanup - Patch table name is now configurable - Patch directory is no longer ignored - Autopatch.run callback now returns count of new patches applied - Removed console logging from module

--- examples/testClient.js
+++ examples/testClient.js
@@ -1,21 +1,24 @@
+var path = require('path');
 var autopatch = require('../lib/autopatch.js');
 
 var config = {
-	patchDir: './patches/',
+	patchDir: path.resolve(__dirname, './patches/'),
+    dbType: 'postgres',
 	db: {
 		host: 'localhost',
 		port: 5432,
 		user: 'postgres',
 		database: 'autopatch_test',
-		password: 'password'
+		password: 'password',
+		patchTableName: 'patches'
 	}
 };
 
-autopatch.run(config, function(err) {
+autopatch.run(config, function(err, newPatchCount) {
 	if (err) {
         console.log('autopatch error: ', err);
 	}
 	
-	console.log('autopatch done');
+	console.log('autopatch done. %d new patches applied', newPatchCount);
 	process.exit();
 });
